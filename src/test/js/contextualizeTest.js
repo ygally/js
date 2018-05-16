8530bc2dda8a7c78aa6f58237772ab2ad7101abc
Yannick GALLY <ygally@gmail.com>
2018-05-16 08:44:55 +0200

    Contextualize : added single definition test

--- src/test/js/contextualizeTest.js
+++ src/test/js/contextualizeTest.js
@@ -21,38 +21,50 @@ var defs = [
   {name: "square", req: "res", type: 'int', fmt: square}
 ];
 
-test("tpl txt fmt", (a) => {
+test("tpl txt fmt", a => {
 	   var R = contextualize(data).translate(defs);
     a.equals(R.res, "root/test/yg.nfo",
     	  "res should be the en path");
 });
 
-test("twice op1 fmt", (a) => {
+test("twice op1 fmt", a => {
 	   var R = contextualize(data).translate(defs);
     a.equals(R.double, "4 + 4 = 8",
     	  "doudle should show 4+4 op");
 });
 
-test("old required car", (a) => {
+test("old required car", a => {
 	   var R = contextualize(data).translate(defs);
     a.equals(R.str, "Bentley",
     	  "simple car should be Bentley");
 });
 
-test("old double required car", (a) => {
+test("old double required car", a => {
 	   var R = contextualize(data).translate(defs);
     a.equals(R.required, "Ferrari",
     	  "car should have fell to Ferrari");
 });
 
-test("number in str", (a) => {
+test("number in str", a => {
 	   var R = contextualize(data).translate(defs);
     a.equals(R.num, "4",
     	  "number in text format should result in string");
 });
 
-test("string nb into nb", (a) => {
+test("string nb into nb", a => {
 	   var R = contextualize(data).translate(defs);
     a.equals(R.square, 64,
     	  "should get nb 8x8 product");
+});
+
+test("single definition", a => {
+	   var rabbit = contextualize({
+	   	    vegetables: 'salad',
+	   	    roots: 'carrot'
+	   	}).translate({
+	   		   name: 'food',
+	   		   text: '{{roots}}&{{vegetables}}'
+	   	});
+    a.equals(rabbit.food, 'carrot&salad',
+    	  "rabbit food is carrot&salad");
 });
