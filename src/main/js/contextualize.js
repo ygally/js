b0928f4424fe5cc7a0ed8be988da5b714c52fd76
Yannick GALLY <ygally@gmail.com>
2018-05-16 08:30:46 +0200

    Contextualize : fixed definition alteration + added support for single definition

--- src/main/js/contextualize.js
+++ src/main/js/contextualize.js
@@ -3,9 +3,9 @@ var LEN = 'length',
     DEFAULT_RE = new RegExp(RE_FMT.replace("K", "([^}]+)")),
     UNDEFINED,
     isArray = Array.isArray;
-function collect(obj){
+function collector(obj) {
     return function(kvPair) {
-        obj[kvPair.key] = kvPair.value;
+        obj[kvPair[0]] = kvPair[1];
     };
 }
 function get(D, req) {
@@ -15,7 +15,7 @@ function get(D, req) {
                 var R = get(D, req[0]);
                 return R !== UNDEFINED?
                     R:
-                    get(D,req.splice(1));
+                    get(D, req.slice(1));
             }
             return UNDEFINED;
         } 
@@ -35,22 +35,21 @@ function expFrom(D, T, RE) {
     return T;
 }
 function contextualize(D, RE) {
-    function translate(def) {
+    function innerTranslate(def) {
         if (isArray(def)) {
             var accu = {};
-            def.map(translate)
-                .forEach(collect(accu));
+            def.map(innerTranslate)
+                .forEach(collector(accu));
             return accu;
         } 
-        return {
-            key: def.name,
-            value: def.text ?
+        return [def.name, def.text ?
                 expFrom(D, def.text, RE || DEFAULT_RE):
-                get(D, def.req)
-        };
+                get(D, def.req)];
     }
     return {
-        translate: translate
+        translate: function translate(def) {
+        	   return innerTranslate(isArray(def)? def: [def]);
+        }
     };
 }
 
