7ff3b53d0ccf4d81812a61eb7883d8b49bd7348e
Yannick GALLY <ygally@gmail.com>
2018-07-30 21:11:51 +0200

    Require by YG : enhance testing system using YG 'test' tool + enhance Node-like module definition (by removing closure anonymous function)

--- src/main/js/require.js
+++ src/main/js/require.js
@@ -1,84 +1,86 @@
 var Promise = require('../../main/js/Promise'),
     NIL,
+    modules = {},
+	   downloads = {},
     RE_PROVIDE = /^provide:/;
-function defaultRemoteLoad(name) {
+function remoteLoad(name) {
 	   throw 'No module "' + name + '" provided';
 }
-var main = (function(remoteLoad, modules, downloads) {
-	   modules = {};
-	   downloads = {};
-	   function onDepsError(err) {
-	       console.log('dependency error: ' + err);
-	       return Promise.reject(err);
-	   }
-	   function retrieveDep(name) {
-	   	    if (!modules[name]) {
-	   	    	   if (!downloads[name]) {
-	   	    	   	    downloads[name] = Promise.resolve(remoteLoad(name));
-	   	    	   	}
-	   	    	   return downloads[name]
-	   	    	      .then(function afterDL() {
-	   	    	      	    return modules[name];
-	   	    	      	}).or(onDepsError);
-	   	    }
-	   	    return modules[name];
-	   }
-	   var mainPromiseResolver,
-	       mainPromise;
-    function execute(name,dep,define) {
-    	    if (RE_PROVIDE.test(name)) {
-    	     	   name = name.replace(RE_PROVIDE, '');
-    	    } else {
-    	    	    // this case represents a simple use
-    	    	    // not a definition
-    	    	    define = dep;
-    	    	    dep = name;
-    	    	    name = NIL;
-    	    }
-    	    var definition;
-    	    if (typeof dep === 'function') {
-    	    	   define = dep;
-    	    	   dep = [];
-    	    } else if (Array.isArray(dep)) {
-    	    	   dep = dep.map(retrieveDep);
-    	    } else {
-    	    	   dep = [retrieveDep(dep)];
-	        }
-	        dep = [mainPromise].concat(dep);
-	        function onDepsReadyExec(deps) {
-	        	   //console.log('after resolved ' + deps.length + ' deps' + (name? ' [' + name + ']': ''));
-	        	   return define.apply(NIL, deps);
-	        }
-	        var allDepsReady = Promise.all(dep, 'all_' + name + '_deps');
-	        //console.log('deps resolver promise : ' + allDepsReady.name);
-	        if (name) {
-	        	   //console.log('providing ' + name + '... ' + dep.length + ' deps');
-	        	   function definitionResolver(fulfill, reject) {
-	        	   	    allDepsReady
-	        	   	        .then(onDepsReadyExec)
-	                    .then(function definitionResolve(definition) {
-	                        //console.log('> Defined ' + name);
-	       	                 fulfill(definition);
-	                    })
-	                    .or(reject);
-	        	   	}
-	        	   modules[name] = new Promise(definitionResolver, name + 'Promise');
-	        } else {
-	        	   //console.log('simple execution... ' + dep.length + ' deps');
-	        	   allDepsReady
-	                .then(onDepsReadyExec)
-	                .or(onDepsError);
-	        }
-    }
-    mainPromiseResolver = function mainPromiseResolver(fulfill) {
-    	   fulfill(execute);
-	   };
-	   mainPromise = new Promise(mainPromiseResolver, 'mainDep');
-	   execute.setRemoteLoader = function setRemoteLoader(rl) {
-	   	    remoteLoad = rl;
+function createDepErrorHandler(fail) {
+	   return fail? function handleDepError(err) {
+	   	    fail(err);
+	   	}: function handleDepError(err) {
+	   	    throw 'Dependency Error: ' + err;
 	   	};
-    return execute;
-}(defaultRemoteLoad));
+}
+function retrieveDep(name) {
+	   if (!modules[name]) {
+	   	    if (!downloads[name]) {
+	   	    	   downloads[name] = Promise.resolve(remoteLoad(name));
+	   	    }
+	   	    return downloads[name]
+	   	    	   .then(function afterDL() {
+	   	    	       return modules[name];
+	   	    	   }).or(createDepErrorHandler());
+	   }
+	   	return modules[name];
+}
+var mainPromiseResolver,
+    mainPromise;
+function main(name, dep, define, fail) {
+    if (RE_PROVIDE.test(name)) {
+     	   name = name.replace(RE_PROVIDE, '');
+    } else {
+    	   // this case represents a simple use
+        // not a definition
+        fail = define;
+    	   define = dep;
+    	   dep = name;
+    	   name = NIL;
+    }
+    var definition;
+    if (typeof dep === 'function') {
+    	   fail = define;
+    	   define = dep;
+    	   dep = [];
+    } else if (Array.isArray(dep)) {
+    	   dep = dep.map(retrieveDep);
+    } else {
+    	   dep = [retrieveDep(dep)];
+	   }
+	   dep = [mainPromise].concat(dep);
+	   function onDepsReadyExec(deps) {
+	       //console.log('after resolved ' + deps.length + ' deps' + (name? ' [' + name + ']': ''));
+	       return define.apply(NIL, deps);
+	   }
+	   var allDepsReady = Promise.all(dep, 'all_' + name + '_deps');
+	   //console.log('deps resolver promise : ' + allDepsReady.name);
+	   if (name) {
+	       //console.log('providing ' + name + '... ' + dep.length + ' deps');
+	       function definitionResolver(resolve, reject) {
+	        	   allDepsReady
+	        	   	   .then(onDepsReadyExec)
+	               .then(function definitionResolve(definition) {
+	                   //console.log('> Defined ' + name);
+	       	            resolve(definition);
+	               })
+	               .or(reject);
+	       	}
+	       modules[name] = new Promise(definitionResolver, name + 'Promise');
+	   } else {
+	       //console.log('simple execution... ' + dep.length + ' deps');
+	       allDepsReady
+	           .then(onDepsReadyExec)
+	           .or(createDepErrorHandler(fail));
+	   }
+}
+mainPromiseResolver = function mainPromiseResolver(resolve) {
+ 	  resolve(main);
+};
+mainPromise = new Promise(mainPromiseResolver, 'mainDep');
+main.setRemoteLoader = function setRemoteLoader(rl) {
+	   remoteLoad = rl;
+};
 if (module) {
     module.exports = main;
 }
--- src/test/js/requireCalcTest.js
+++ src/test/js/requireCalcTest.js
@@ -1,42 +1,92 @@
+/*global module */
+var Promise = require('../../main/js/Promise');
 var requoya = require('../../main/js/require');
+var test = require('./test');
+
+EMULATED_FILES = {
+ 	'pawa': function pawa() {
+		   requoya(
+	       'provide:pawa',
+	       function powerDefine(core) {
+	       	    return function power(a, n) {
+	           	    var r = 1;
+	           	    while (n--) {
+	       	        	   r *= a;
+	       	        }
+	           	    return r;
+	           };
+        });
+  }
+};
+
+function emulatedLoad(name) {
+	   return new Promise(function loadPromiseDef(resolve, reject) {
+	   	    setTimeout(function delayedEmulatedFile() {
+	   	    	   try{
+	   	    	   	  resolve(EMULATED_FILES[name]());
+	   	    	   } catch(e) {
+	   	    	   	  reject(e + ' [name=' + name + ']');
+	   	    	   }
+	   	    }, 3000);
+	   	});
+}
+requoya.setRemoteLoader(emulatedLoad);
+
 requoya(
 	   'provide:adder',
 	   function addDefine(core) {
 	       return function add(a, b) {
-	 	         return a + b;
+	       	    return a + b;
 	       };
     });
-requoya(
-  	 ['adder'],
-  	 function simpleUse1(core, add, mult) {
-	     	 console.log('42 + 43 => ' + add(42, 43));
-   });
+
+test('require adder', function(a) {
+    requoya(
+        ['adder'],
+        function simpleUse1(core, add) {
+            a.equals(add(42, 43), 85, '42+43 => 85');
+            a.end();
+        });
+});
+
 requoya(
 	   'provide:multiplier',
 	   function multDefine(core) {
 	       return function mult(a, b) {
-	 	          return a * b;
+	       	    return a * b;
 	       };
     });
-requoya(
-  	 ['adder', 'multiplier'],
-  	 function simpleUse1(core, add, mult) {
-	     	 console.log('4 + 6 => ' + add(4, 6));
-	  	    console.log('4 * 6 => ' + mult(4, 6));
-   });
-requoya(
-	   'provide:pawa',
-	   function powerDefine(core) {
-	   	    return new Promise(function(giveDef) {
-	           setTimeout(giveDef.bind(undefined, function power(a, n) {
-	       	        var r = 1;
-	       	        while (n--) {
-	       	        	   r *= a;
-	       	        }
-	       	        return r;
-	           }), 2000);
-	       });
-    });
+
+test('require multiplier', function(a) {
+    requoya(
+    	   ['multiplier'],
+    	   function simpleUse2(core, mult) {
+            a.equals(mult(3, 9), 27, '3x9 => 27');
+            a.end();
+        });
+});
+
+test('require adder&multiplier', function(a) {
+    requoya(
+    	   ['adder', 'multiplier'],
+    	   function simpleUse3(core, add, mult) {
+            a.equals(add(4, 6), 10, '4+6 => 10');
+            a.equals(mult(4, 6), 24, '4x6 => 24');
+            a.end();
+        });
+});
+
+test('require adder&multiplier&divide', function(a) {
+    requoya(
+    	   ['adder', 'multiplier', 'divid'],
+    	   function simpleUse4() {
+    	   	    a.fail();
+        }, function(err) {
+        	   a.equals(err.indexOf('Dependency Error'), 0, 'Should receive a dependency error');
+        	   a.end();
+        });
+});
+
 requoya(
   	 'provide:calc',
 	   ['adder', 'multiplier', 'pawa'],
@@ -44,15 +94,24 @@ requoya(
 	       return {
 	       	    add: add,
 	       	    mult: mult,
-	       	    power: power
+	       	    power: power,
+	       	    sqr: function square(x) {
+	       	    	   return power(x, 2);
+	       	    }
 	       	};
     });
-requoya(
-	   'calc',
-	   function calcUse1(core, calc) {
-        console.log("3 et 6 : " + calc.add(3, 6));
-        console.log("4 x 7 : " + calc.mult(4, 7));
-        console.log("8^0 : " + calc.power(8, 0));
-        console.log("12^2 : " + calc.power(12, 2));
-        console.log("3^3 : " + calc.power(3, 3));
-    });
+
+test('require calc', function(a) {
+    requoya(
+	       'calc',
+	       function calcUse1(core, calc) {
+	       	    a.equals(calc.add(3, 6), 9, '3 et 6 : 9');
+            a.equals(calc.mult(4, 7), 28, '4 x 7 : 28');
+            a.equals(calc.power(8, 0), 1, '8 ^ 0 : 1');
+            a.equals(calc.power(12, 1), 12, '12 ^ 1 : 12');
+            a.equals(calc.power(7, 2), 49, '7 ^ 2 : 49');
+            a.equals(calc.power(3, 3), 27, '3 ^ 3 : 27');
+            a.equals(calc.sqr(11), 121, '11^2 : 121');
+            a.end();
+	   	    });
+	   	});
