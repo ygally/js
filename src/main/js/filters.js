c667f76d281c6286de63d8d53b02c3def66610e9
Yannick GALLY <ygally@gmail.com>
2018-08-11 11:39:46 +0200

    Filters by YG : added support for filter groups (from objects properties)

--- src/main/js/filters.js
+++ src/main/js/filters.js
@@ -1,21 +1,44 @@
+/*
+	 FIXME 001 : allow multiple instances
+	 FIXME 002 : allow more than 1 init call
+*/
 var yareq = require('../../main/js/require');
 
 yareq(
 	   'provide:filters',
 	   function filtersDefinition(core) {
-	   	    var filters = [];
-	   	    var filterMap = {};
-	   	    var originals = [];
+	   	    var filters,
+	   	        filterMap,
+	   	        groups,
+	   	        groupMap,
+	   	        originals;
+	   	    function resetAll() {
+	   	        filters = [];
+	   	        filterMap = {};
+	   	        groups = [];
+	   	        groupMap = {};
+	   	        originals = [];
+	   	    }
+	   	    resetAll();
 	   	    function resetOne(filtr) {
 	   	        filtr.index = [];
+	       }
+	       function resetOneGroup(g) {
+	   	        g.values = [];
 	       }
 	       function originalFromIndex(i) {
             return originals[i];
         }
         function originalsFrom(indexArray) {
-            return indexArray.map(originalFromIndex);
+            return indexArray && indexArray.map(originalFromIndex);
         }
 	       function create(name, accept) {
+	       	    if (!name) {
+	       	        throw 'create(): need name for filter';
+	       	    }
+	       	    if (filterMap[name]) {
+	       	        throw 'create(): filter already exists for name "' + name + '"';
+	       	    }
             var f = {
                 "name": name,
                 "accept": accept,
@@ -23,7 +46,48 @@ yareq(
             };
             filters.push(f);
             filterMap[name] = f;
+            //console.log('created filter', f.name);
 	       }
+	       function isFunction(f) {
+            return typeof f === 'function';
+        }
+	       function createByProperty(prefix, property) {
+	       	    if (!prefix) {
+	       	        throw 'createByProperty(): need name prefix for filter group';
+	       	    }
+	       	    if (groupMap[prefix]) {
+	       	        throw 'createByProperty(): filter group already exists for prefix "' + prefix + '"';
+	       	    }
+	       	    property = property || prefix;
+	       	    var getValue = property;
+	       	    if (!isFunction(getValue)) {
+	       	        //console.log('creating property retriever for "' + property + '"');
+	       	    	   getValue = function getValue(o) {
+	       	    	   	    return o[property];
+	       	        };
+	       	    }
+	       	    fGroup = {
+	       	        "prefix": prefix,
+	       	        "getValue": getValue,
+	       	        "values": []
+	       	    };
+	       	    groups.push(fGroup);
+            groupMap[prefix] = fGroup;
+            //console.log('created filter group ' + fGroup.prefix + ':...');
+	       	}
+	       function createGroupFiltersFrom(obj) {
+	       	    function createIfNewForGroup(g) {
+	       	        var value = g.getValue(obj),
+	       	            name = g.prefix + ':' + value;
+	       	        if (!filterMap[name]) {
+	       	            g.values.push(value);
+	       	            create(name, function acceptOneVal(o) {
+	       	                return value === g.getValue(o);
+	       	            });
+	       	        }
+	       	    }
+	       	    groups.forEach(createIfNewForGroup);
+	       	}
 	       function createIndexes(object, i) {
 	       	    filters.forEach(function createIndex(f) {
 	       	        if (f.accept(object)) {
@@ -33,18 +97,39 @@ yareq(
 	       	}
 	       function initWith(objects) {
 	       	    originals = objects;
+	       	    groups.forEach(resetOneGroup);
+	       	    originals.forEach(createGroupFiltersFrom);
 	       	    filters.forEach(resetOne);
 	       	    originals.forEach(createIndexes);
 	       	}
-	       	function indexesFor(name) {
-	       	    return filterMap[name].index;
+	       	function names(prefix) {
+	       	    var list = [];
+	       	    prefix = prefix && new RegExp('^' + prefix + ':');
+	       	    filters.forEach(function getNameOf(f) {
+	       	        if (!prefix || prefix.test(f.name)) {
+	       	            list.push(f.name);
+	       	        }
+	       	    });
+	       	    return list;
 	       	}
-	       	function objectsFor(name) {
-	       	    return originalsFrom(filterMap[name].index);
+	       	function valuesOf(fGroup) {
+	       	    fGroup = fGroup && groupMap[fGroup];
+	       	    return fGroup && fGroup.values;
+	       	}
+	       	function indexesFor(f) {
+	       	    f = f && filterMap[f];
+	       	    return f && f.index;
+	       	}
+	       	function objectsFor(f) {
+	       	    return originalsFrom(indexesFor(f));
 	       	}
 	       return {
+	       	    "reset": resetAll,
 	       	    "create": create,
+	       	    "names": names,
+	       	    "byProperty": createByProperty,
 	       	    "initWith": initWith,
+	       	    "valuesOf": valuesOf,
 	       	    "indexesFor": indexesFor,
 	       	    "objectsFor": objectsFor
 	       	};
--- src/test/js/filtersTest.js
+++ src/test/js/filtersTest.js
@@ -1,13 +1,17 @@
-/*global module */
+/*global require */
 var yareq = require('../../main/js/require');
 var yatest = require('./test');
 
 require('../../main/js/filters');
+function numerically(a, b) {
+    return Math.sign(a-b);
+}
 
 yatest('simple filters', function(a) {
     yareq(
         ['filters'],
         function simpleFilters(core, filters) {
+            filters.reset();
             filters.create('matchA', o => o.name.indexOf('a') >= 0);
             filters.create('matchE', o => o.name.indexOf('e') >= 0);
             filters.create('matchR', o => o.name.indexOf('r') >= 0);
@@ -22,4 +26,32 @@ yatest('simple filters', function(a) {
         	   a.equals(filters.objectsFor('matchR').map(o=>o.name).join("_"), "spray_present");
         	   a.end();
         });
+});
+
+yatest('based on value filters', function(a) {
+    yareq(['filters'], function valueFilters(core, filters) {
+        filters.reset();
+        filters.byProperty('category');
+        filters.byProperty('age', 'years');
+        filters.byProperty('cute', o => o.isCute && o.isCute() || !1);
+        filters.byProperty('cap', o => o.name.charAt(0).toUpperCase());
+        var cats = [
+            {name: 'Cuttie', category: 'Mau égyptien', years: 12, isCute: () => true},
+            {name: 'Akira', category: 'Himalayen', years: 2},
+            {name: 'Chewbacca', category: 'Siamois', years: 4},
+            {name: 'Alex', category: 'Mau égyptien', years: 4, isCute: () => true},
+            {name: 'Mioumiou', category: 'Himalayen', years: 4, isCute: () => true}
+        ];
+        filters.initWith(cats);
+        var expectedNames = 'age:12;age:2;age:4';
+        expectedNames += ';cap:A;cap:C;cap:M';
+        expectedNames += ';category:Himalayen;category:Mau égyptien;category:Siamois';
+        expectedNames += ';cute:false;cute:true';
+        a.equals(filters.names().sort().join(";"), expectedNames);
+        a.equals(filters.valuesOf('category').sort().join(";"), "Himalayen;Mau égyptien;Siamois");
+        a.equals(filters.valuesOf('age').sort(numerically).join(";"), '2;4;12');
+        a.equals(filters.indexesFor('category:Siamois').join("_"), "2");
+        a.equals(filters.objectsFor('category:Mau égyptien').map(o=>o.name).join("_"), "Cuttie_Alex");
+    	   a.end();
+    });
 });
