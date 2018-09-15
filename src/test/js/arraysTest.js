47ade96d1867e3ab1063bac707e4a97e21599b2c
Yannick GALLY <ygally@gmail.com>
2018-09-15 17:40:20 +0200

    Arrays utils by YG : added tests cases for intersection with empty arrays

--- src/test/js/arraysTest.js
+++ src/test/js/arraysTest.js
@@ -11,6 +11,9 @@ yatest('numeric intersections', function(a) {
         	   a.equals(inter([42, 43],[42]).join("_"), "42");
         	   a.equals(inter([1,2,3,4,5],[2,4]).join("_"), "2_4");
         	   a.equals(inter([1,4,8,14],[1,5,6,8]).join(","), "1,8");
+        	   a.equals(inter([42, 43],[]).join(), "");
+        	   a.equals(inter([],[]).join(), "");
+        	   a.equals(inter([], [5]).join(), "");
         	   a.end();
         });
 });
