ffa43d3965a92080bfd3c9001588d05c3412515f
Yannick GALLY <ygally@gmail.com>
2018-09-20 09:41:33 +0200

    Arrays by YG : added support for arrays UNION

--- src/main/js/arrays.js
+++ src/main/js/arrays.js
@@ -1,9 +1,27 @@
-var yareq = require('../../main/js/require');
-yareq('provide:arrays', function arrays(core) {
-	   function intersection(A, B) {
-        return A.filter(e => B.indexOf(e) >= 0);
-	   }
-	   return {
-	       intersection: intersection
+/*global module*/
+var NIL;
+function contained(array, element) {
+    return array.indexOf(element) >= 0;
+}
+contained.into = function containedInto(array) {
+    return contained.bind(NIL, array);
+}
+function missing(array, element) {
+    return array.indexOf(element) < 0;
+}
+missing.from = function missingFrom(array) {
+    return missing.bind(NIL, array);
+}
+
+function intersection(A, B) {
+    return A.filter(contained.into(B));
+}
+function union(A, B) {
+    return A.concat(B.filter(missing.from(A)));
+}
+if (module) {
+    module.exports = {
+	       intersection: intersection,
+	       union: union
 	   };
-});
+}
--- src/test/js/arraysTest.js
+++ src/test/js/arraysTest.js
@@ -1,19 +1,24 @@
-var yareq = require('../../main/js/require');
+var arrays = require('../../main/js/arrays');
 var yatest = require('./test');
 
-require('../../main/js/arrays');
-
 yatest('numeric intersections', function(a) {
-    yareq(
-        ['arrays'],
-        function numInter(core, arrays) {
-            var inter = arrays.intersection;
-        	   a.equals(inter([42, 43],[42]).join("_"), "42");
-        	   a.equals(inter([1,2,3,4,5],[2,4]).join("_"), "2_4");
-        	   a.equals(inter([1,4,8,14],[1,5,6,8]).join(","), "1,8");
-        	   a.equals(inter([42, 43],[]).join(), "");
-        	   a.equals(inter([],[]).join(), "");
-        	   a.equals(inter([], [5]).join(), "");
-        	   a.end();
-        });
+    var inter = arrays.intersection;
+    a.equals(inter([42, 43],[42]).join("_"), "42");
+    a.equals(inter([1,2,3,4,5],[2,4]).join("_"), "2_4");
+    a.equals(inter([1,4,8,14],[1,5,6,8]).join(","), "1,8");
+	   a.equals(inter([42, 43],[]).join(), "");
+    a.equals(inter([],[]).join(), "");
+    a.equals(inter([], [5]).join(), "");
+    a.end();
+});
+
+yatest('numeric union', function(a) {
+    var union = arrays.union;
+    a.equals(union([42, 43],[42]).join("_"), "42_43");
+    a.equals(union([1,3,4,5],[2,4]).join("_"), "1_3_4_5_2");
+    a.equals(union([1,14],[5,6,8]).join(","), "1,14,5,6,8");
+	   a.equals(union([4, 3],[]).join(','), "4,3");
+    a.equals(union([],[]).join(), "");
+    a.equals(union([], [5]).join(), "5");
+    a.end();
 });
