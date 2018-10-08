9f0ef2d04614878bb81d493e619658fbc8f5b68a
Yannick GALLY <ygally@gmail.com>
2018-10-08 19:59:34 +0200

    Sorts by YG : better tests presentation

--- src/test/js/sortsTest.js
+++ src/test/js/sortsTest.js
@@ -1,20 +1,21 @@
-var sorts = require('../../main/js/sorts');
+var test = module.require('./test');
+var sorts = module.require('../../main/js/sorts');
 var comparator = sorts.comparator;
-var yatest = require('./test');
 
 function Sportif(nb, name, pos) {
     this.nb = nb;
     this.name = name;
     this.pos = pos;
 }
+const players = [
+        new Sportif(231, 'Billy', 3),
+        new Sportif(456, 'Arnold', 2),
+        new Sportif(47, 'Anna', 4),
+        new Sportif(398, 'Gregory', 1),
+    ];
 
-yatest('sort by name,nb,pos', function(a) {
-    var data = [
-            new Sportif(231, 'Billy', 3),
-            new Sportif(456, 'Arnold', 2),
-            new Sportif(47, 'Anna', 4),
-            new Sportif(398, 'Gregory', 1),
-        ];
+test('sort by name,nb,pos', function(a) {
+    var data = players.slice();
     data.sort(comparator.by('name'));
     a.equals(data.map(o=>o.name).join("_"), "Anna_Arnold_Billy_Gregory");
     data.sort(comparator.by('nb'));
@@ -24,13 +25,8 @@ yatest('sort by name,nb,pos', function(a) {
     a.end();
 });
 
-yatest('sort by getter', function(a) {
-    var data = [
-            new Sportif(231, 'Billy', 3),
-            new Sportif(456, 'Arnold', 2),
-            new Sportif(47, 'Anna', 4),
-            new Sportif(398, 'Gregory', 1),
-        ];
+test('sort by getter', function(a) {
+    var data = players.slice();
     data.sort(comparator.by(o => o.name.length));
     a.equals(data.map(o=>o.name).join("_"), "Anna_Billy_Arnold_Gregory");
     a.end();
