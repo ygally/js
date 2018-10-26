b36e7d86c902478e090ddba3d22937a94a84341c
Yannick GALLY <ygally@easyvoyage.fr>
2018-10-26 10:02:42 +0200

    Packager by YG : removed tab chars

--- src/main/js/yacage.js
+++ src/main/js/yacage.js
@@ -12,85 +12,85 @@
 var Promise = module.require('./Promise'),
     NIL,
     modules = {},
-	   downloads = {},
+       downloads = {},
     RE_PROVIDE = /^provide:/,
     load;
 function defaultLoad(name) {
-	   throw 'No module "' + name + '" provided';
+       throw 'No module "' + name + '" provided';
 }
 function setExternalLoad(l) {
-	   load = l;
+       load = l;
 }
 load = defaultLoad;
 function createDepErrorHandler(fail) {
-	   return fail? function handleDepError(err) {
-	   	    fail(err);
-	   	}: function handleDepError(err) {
-	   	    throw 'Dependency Error: ' + err;
-	   	};
+       return fail? function handleDepError(err) {
+            fail(err);
+        }: function handleDepError(err) {
+            throw 'Dependency Error: ' + err;
+        };
 }
 function retrieveDep(name) {
-	   if (!modules[name]) {
-	   	    if (!downloads[name]) {
-	   	    	   downloads[name] = Promise.resolve(load(name));
-	   	    }
-	   	    return downloads[name]
-	   	    	   .then(function afterDL() {
-	   	    	       return modules[name];
-	   	    	   }).or(createDepErrorHandler());
-	   }
-	   	return modules[name];
+       if (!modules[name]) {
+            if (!downloads[name]) {
+                   downloads[name] = Promise.resolve(load(name));
+            }
+            return downloads[name]
+                   .then(function afterDL() {
+                       return modules[name];
+                   }).or(createDepErrorHandler());
+       }
+        return modules[name];
 }
 var cagePromiseResolver,
     cagePromise;
 function cage(name, dep, define, fail) {
     if (RE_PROVIDE.test(name)) {
-     	   name = name.replace(RE_PROVIDE, '');
+           name = name.replace(RE_PROVIDE, '');
     } else {
-    	   // this case represents a simple use
+           // this case represents a simple use
         // not a definition
         fail = define;
-    	   define = dep;
-    	   dep = name;
-    	   name = NIL;
+           define = dep;
+           dep = name;
+           name = NIL;
     }
     var definition;
     if (typeof dep === 'function') {
-    	   fail = define;
-    	   define = dep;
-    	   dep = [];
+           fail = define;
+           define = dep;
+           dep = [];
     } else if (Array.isArray(dep)) {
-    	   dep = dep.map(retrieveDep);
+           dep = dep.map(retrieveDep);
     } else {
-    	   dep = [retrieveDep(dep)];
-	   }
-	   dep = [cagePromise].concat(dep);
-	   function defineModuleWith(deps) {
-	       try{
-	       	    return define.apply(NIL, deps);
-	       	} catch(e) {
-	       		   console.error('Error while defining/executing module ' + (name? '"' + name + '" ': ''), e);
-	       	}
-	   }
-	   var allDepsReady = Promise.all(dep, 'all_' + name + '_deps');
-	   if (name) {
-	       function definitionResolver(resolve, reject) {
-	        	   allDepsReady
-	        	   	   .then(defineModuleWith)
-	               .then(function definitionResolve(definition) {
-	                   resolve(definition);
-	               })
-	               .or(reject);
-	       	}
-	       modules[name] = new Promise(definitionResolver, name + 'Promise');
-	   } else {
-	       allDepsReady
-	           .then(defineModuleWith)
-	           .or(createDepErrorHandler(fail));
-	   }
+           dep = [retrieveDep(dep)];
+       }
+       dep = [cagePromise].concat(dep);
+       function defineModuleWith(deps) {
+           try{
+                return define.apply(NIL, deps);
+            } catch(e) {
+                   console.error('Error while defining/executing module ' + (name? '"' + name + '" ': ''), e);
+            }
+       }
+       var allDepsReady = Promise.all(dep, 'all_' + name + '_deps');
+       if (name) {
+           function definitionResolver(resolve, reject) {
+                   allDepsReady
+                       .then(defineModuleWith)
+                   .then(function definitionResolve(definition) {
+                       resolve(definition);
+                   })
+                   .or(reject);
+            }
+           modules[name] = new Promise(definitionResolver, name + 'Promise');
+       } else {
+           allDepsReady
+               .then(defineModuleWith)
+               .or(createDepErrorHandler(fail));
+       }
 }
 cagePromiseResolver = function cagePromiseResolver(resolve) {
- 	  resolve(cage);
+      resolve(cage);
 };
 cagePromise = new Promise(cagePromiseResolver, 'mainDep');
 cage.setExternalLoad = setExternalLoad;
--- src/test/js/yacageTest.js
+++ src/test/js/yacageTest.js
@@ -4,28 +4,28 @@ var Promise = module.require('../../main/js/Promise');
 var cage = module.require('../../main/js/yacage');
 
 EMULATED_REMOTE = {
- 	'pawa': function pawa() {
-		   cage(
-	       'provide:pawa',
-	       function powerDefine(core) {
-	       	    return function power(a, n) {
-	           	    var r = 1;
-	           	    while (n--) {
-	       	        	   r *= a;
-	       	        }
-	           	    return r;
-	           };
+    'pawa': function pawa() {
+           cage(
+           'provide:pawa',
+           function powerDefine(core) {
+                return function power(a, n) {
+                    var r = 1;
+                    while (n--) {
+                           r *= a;
+                    }
+                    return r;
+               };
         });
   }
 };
 EMULATED_CACHE = {
- 	'substract': function substractLoader() {
-		   cage(
-	       'provide:substract',
-	       function substractDefine(core) {
-	       	    return function substract(a, b) {
-	           	    return a - b;
-	           };
+    'substract': function substractLoader() {
+           cage(
+           'provide:substract',
+           function substractDefine(core) {
+                return function substract(a, b) {
+                    return a - b;
+               };
         });
   }
 };
@@ -33,24 +33,24 @@ function emulatedLoad(name) {
     if (EMULATED_CACHE[name]) {
         return EMULATED_CACHE[name]();
     }
-	   return new Promise(function loadPromiseDef(resolve, reject) {
-	   	    setTimeout(function delayedEmulatedFile() {
-	   	    	   try{
-	   	    	   	  resolve(EMULATED_REMOTE[name]());
-	   	    	   } catch(e) {
-	   	    	   	  reject(e + ' [name=' + name + ']');
-	   	    	   }
-	   	    }, 1000);
-	   	});
+       return new Promise(function loadPromiseDef(resolve, reject) {
+            setTimeout(function delayedEmulatedFile() {
+                   try{
+                      resolve(EMULATED_REMOTE[name]());
+                   } catch(e) {
+                      reject(e + ' [name=' + name + ']');
+                   }
+            }, 1000);
+        });
 }
 cage.setExternalLoad(emulatedLoad);
 
 cage(
-	   'provide:adder',
-	   function addDefine(core) {
-	       return function add(a, b) {
-	       	    return a + b;
-	       };
+       'provide:adder',
+       function addDefine(core) {
+           return function add(a, b) {
+                return a + b;
+           };
     });
 
 test('require adder', function(a) {
@@ -64,25 +64,25 @@ test('require adder', function(a) {
 
 test('require subtract', function(a) {
     cage(
-    	   ['subtract'],
-    	   function simpleRemove(core, substrat) {
+           ['subtract'],
+           function simpleRemove(core, substrat) {
             a.equals(subtract(8, 3), 5, '8-3 => 5');
             a.end();
         });
 });
 
 cage(
-	   'provide:multiplier',
-	   function multDefine(core) {
-	       return function mult(a, b) {
-	       	    return a * b;
-	       };
+       'provide:multiplier',
+       function multDefine(core) {
+           return function mult(a, b) {
+                return a * b;
+           };
     });
 
 test('require multiplier', function(a) {
     cage(
-    	   ['multiplier'],
-    	   function simpleUse2(core, mult) {
+           ['multiplier'],
+           function simpleUse2(core, mult) {
             a.equals(mult(3, 9), 27, '3x9 => 27');
             a.end();
         });
@@ -90,8 +90,8 @@ test('require multiplier', function(a) {
 
 test('require adder&multiplier', function(a) {
     cage(
-    	   ['adder', 'multiplier'],
-    	   function simpleUse3(core, add, mult) {
+           ['adder', 'multiplier'],
+           function simpleUse3(core, add, mult) {
             a.equals(add(4, 6), 10, '4+6 => 10');
             a.equals(mult(4, 6), 24, '4x6 => 24');
             a.end();
@@ -100,35 +100,35 @@ test('require adder&multiplier', function(a) {
 
 test('require adder&multiplier&divide', function(a) {
     cage(
-    	   ['adder', 'multiplier', 'divid'],
-    	   function simpleUse4() {
-    	   	    a.fail();
+           ['adder', 'multiplier', 'divid'],
+           function simpleUse4() {
+                a.fail();
         }, function(err) {
-        	   a.equals(err.indexOf('Dependency Error'), 0, 'Should receive a dependency error');
-        	   a.end();
+               a.equals(err.indexOf('Dependency Error'), 0, 'Should receive a dependency error');
+               a.end();
         });
 });
 
 test('require calc', function(a) {
     cage(
-      	 'provide:calc',
-	       ['adder', 'substract', 'multiplier', 'pawa'],
-	       function calcDefine(core, add, sub, mult, power) {
-	           return {
-	           	    add: add,
-	           	    sub: sub,
-	           	    mult: mult,
-	           	    power: power,
-	           	    sqr: function square(x) {
-	           	    	   return power(x, 2);
-	           	    }
-    	       	};
+         'provide:calc',
+           ['adder', 'substract', 'multiplier', 'pawa'],
+           function calcDefine(core, add, sub, mult, power) {
+               return {
+                    add: add,
+                    sub: sub,
+                    mult: mult,
+                    power: power,
+                    sqr: function square(x) {
+                           return power(x, 2);
+                    }
+                };
         });
     a.pass('started to provide Calc module');
     cage(
-	       'calc',
-	       function calcUse1(core, calc) {
-	       	    a.equals(calc.add(3, 6), 9, '3 et 6 : 9');
+           'calc',
+           function calcUse1(core, calc) {
+                a.equals(calc.add(3, 6), 9, '3 et 6 : 9');
             a.equals(calc.sub(47, 5), 42, '47-5 : 42');
             a.equals(calc.mult(4, 7), 28, '4 x 7 : 28');
             a.equals(calc.power(8, 0), 1, '8 ^ 0 : 1');
@@ -137,5 +137,5 @@ test('require calc', function(a) {
             a.equals(calc.power(3, 3), 27, '3 ^ 3 : 27');
             a.equals(calc.sqr(11), 121, '11^2 : 121');
             a.end();
-	   	    });
-	   	});
+            });
+        });
